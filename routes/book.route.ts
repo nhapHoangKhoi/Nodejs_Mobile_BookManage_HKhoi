e54c55473b8a2a035eda5da136bdf5db40e10082
NHKhoi <nhaphk@gmail.com>
2025-08-21 03:16:25 +0700

    Get list books with lazy loading

--- routes/book.route.ts
+++ routes/book.route.ts
@@ -45,4 +45,41 @@ router.post("/",
   }
 );
 
+// lazy loading when scrolloing using pagination
+// idea (sent from frontend): 
+// const response = await fetch("http://localhost/api/books?page=1&limit=5");
+//
+router.get("/", 
+  protectRoute, 
+  async (req: Request, res: Response) => {
+    try {
+      const page: number = parseInt(`${req.query.page}`) || 1;
+      const limit: number = parseInt(`${req.query.limit}`) || 2;
+      const skip = (page - 1) * limit;
+
+      const totalRecords = await BookModel.countDocuments();
+      const totalPages = Math.ceil(totalRecords/limit);
+
+      const books = await BookModel.find()
+        .sort({
+          createdAt: "desc"
+        })
+        .skip(skip)
+        .limit(limit)
+        .populate("user", "username profileImage"); // from field "user" in each book record,
+                                                    // next get the correspond username, image
+      res.send({
+        books: books,
+        currentPage: page,
+        totalRecords: totalRecords,
+        totalPages: totalPages,
+      });
+    } 
+    catch (error) {
+      console.log("Error in get all books route", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
+
 export default router;
